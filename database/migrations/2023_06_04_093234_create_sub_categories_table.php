6404403389bba0c8487412054067af4477c61107
bagus.chalil29 <bagus.chalil@gmail.com>
2023-06-04 16:43:38 +0700

    initial commit

--- database/migrations/2023_06_04_093234_create_sub_categories_table.php
+++ database/migrations/2023_06_04_093234_create_sub_categories_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sub_category_tables', function (Blueprint $table) {
+        Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
-            $table->integer('id_category');
-            $table->string('name');
+            $table->integer('id_categories');
+            $table->string('name',150);
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sub_category_tables');
+        Schema::dropIfExists('sub_categories');
     }
 };
